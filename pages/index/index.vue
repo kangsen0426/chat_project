f0613da0f2e80803420cce8de1a49a2584a68e53
“KANG” <2983973848@qq.com>
2022-04-16 22:04:10 +0800

    完成首页tab开发

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,24 @@
 <template>
 	<view>
-		<block v-for="(item,index) in list" :key="index">
-			<common-list :item="item" :index="index" @follow="togglefollow" @togglesupport="doSupport"></common-list>
-			<divider></divider>
-		</block>
+		<scroll-view :scroll-into-view="scrollInto" scroll-with-animation scroll-x="true" class="scroll-row"
+			style="height: 100rpx;">
+			<view v-for="(item,i) in tabBars" :key="i" :id="'tab'+i" class="scroll-row-item px-5 py-2 font-md"
+				:class="tabIndex === i ? 'text-main font-lg font-weight-bold' : ''" @click="changeTab(i)">{{item.name}}
+			</view>
+		</scroll-view>
+		<swiper :duration="500" :current="tabIndex" @change="onchangeTab" :style="'height:'+scrollH+'px'">
+			<swiper-item v-for="(item,i) in newsList" :key="i">
+				<scroll-view scroll-y="true" :style="'height:'+scrollH+'px'">
+					<block v-for="(item2,index) in item.list" :key="index">
+						<common-list :item="item2" :index="index" @follow="togglefollow" @togglesupport="doSupport">
+						</common-list>
+						<divider></divider>
+					</block>
+				</scroll-view>
+			</swiper-item>
+
+		</swiper>
+
 	</view>
 
 </template>
@@ -18,55 +33,92 @@
 		},
 		data() {
 			return {
-				list: [{
-					username: '千北',
-					useravatar: '',
-					newstime: '2022-04-15',
-					isFollow: true,
-					title: '测试',
-					titlepic: '/static/demo/banner1.jpg',
-					support: {
-						type: 'support',
-						support_count: 1,
-						unsupport_count: 2
-					},
-					comment_count: 2,
-					share_num: 2
+				scrollH: 600,
+				tabIndex: 0,
+				scrollInto: 'tab0',
+				tabBars: [{
+					name: '推荐'
 				}, {
-					username: '千北',
-					useravatar: '',
-					newstime: '2022-04-15',
-					isFollow: false,
-					title: '测试',
-					titlepic: '',
-					support: {
-						type: 'unsupport',
-						support_count: 1,
-						unsupport_count: 2
-					},
-					comment_count: 2,
-					share_num: 2
-				}, {
-					username: '千北',
-					useravatar: '',
-					newstime: '2022-04-15',
-					isFollow: false,
-					title: '测试',
-					titlepic: '/static/demo/banner1.jpg',
-					support: {
-						type: '',
-						support_count: 0,
-						unsupport_count: 0
-					},
-					comment_count: 0,
-					share_num: 0
-				}]
+					name: '财经'
+				}],
+				newsList: []
 			}
 		},
 		onLoad() {
-
+			let _this = this
+			let res = uni.getSystemInfo({
+				success(res) {
+					_this.scrollH = res.windowHeight - uni.upx2px(100)
+				}
+			})
+			this.getNewsList()
 		},
 		methods: {
+			getNewsList() {
+				let arr = []
+
+				for (let i = 0; i < this.tabBars.length; i++) {
+					let obj = {
+						list: [{
+							username: '千北',
+							useravatar: '',
+							newstime: '2022-04-15',
+							isFollow: true,
+							title: '测试',
+							titlepic: '/static/demo/banner1.jpg',
+							support: {
+								type: 'support',
+								support_count: 1,
+								unsupport_count: 2
+							},
+							comment_count: 2,
+							share_num: 2
+						}, {
+							username: '千北',
+							useravatar: '',
+							newstime: '2022-04-15',
+							isFollow: false,
+							title: '测试',
+							titlepic: '',
+							support: {
+								type: 'unsupport',
+								support_count: 1,
+								unsupport_count: 2
+							},
+							comment_count: 2,
+							share_num: 2
+						}, {
+							username: '千北',
+							useravatar: '',
+							newstime: '2022-04-15',
+							isFollow: false,
+							title: '测试',
+							titlepic: '/static/demo/banner1.jpg',
+							support: {
+								type: '',
+								support_count: 0,
+								unsupport_count: 0
+							},
+							comment_count: 0,
+							share_num: 0
+						}]
+					}
+					arr.push(obj)
+				}
+				
+				this.newsList = arr
+			},
+			onchangeTab(e) {
+				this.changeTab(e.detail.current)
+			},
+			changeTab(index) {
+				if (this.tabIndex === index) {
+					return
+				}
+				this.tabIndex = index
+				this.scrollInto = 'tab' + index
+
+			},
 			togglefollow(index) {
 
 
