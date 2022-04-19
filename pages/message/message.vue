0bec029b32c0d4126d374bd5f2c913b85f6fcb7c
“KANG” <2983973848@qq.com>
2022-04-19 22:12:59 +0800

    消息列表页

--- pages/message/message.vue
+++ pages/message/message.vue
@@ -1,15 +1,113 @@
 <template>
 	<view>
-		
+		<!-- 消息列表 -->
+		<template v-if="list.length > 0">
+			<!-- 消息列表 -->
+			<block v-for="(item,index) in list" :key="index">
+				<msg-list :item="item" :index="index"></msg-list>
+			</block>
+		</template>
+		<template v-else>
+			<no-thing></no-thing>
+		</template>
+
+		<!-- 弹出层 -->
+		<uni-popup ref="popup" type="top">
+			<view class="flex align-center justify-center font-md border-bottom py-2" hover-class="bg-light"
+				@click="popupEvent('friend')">
+				<text class="iconfont icon-sousuo mr-2"></text> 添加好友
+			</view>
+			<view class="flex align-center justify-center font-md py-2" hover-class="bg-light"
+				@click="popupEvent('clear')">
+				<text class="iconfont icon-shanchu mr-2"></text> 清除列表
+			</view>
+		</uni-popup>
+
 	</view>
 </template>
 
 <script>
+	import msgList from '@/components/message/msgList.vue';
+	import noThing from '@/components/common/nothing.vue';
+	import uniPopup from '@/components/uni-ui/uni-popup/uni-popup.vue';
 	export default {
+		components: {
+			msgList,
+			noThing,
+			uniPopup
+
+		},
 		data() {
 			return {
-				
+				list: [{
+					username: '千北',
+					avatar: '/static/default.jpg',
+					update_time: '1570718427',
+					noread: 3,
+					data: '你好啊~'
+				}, {
+					username: '千北',
+					avatar: '/static/default.jpg',
+					update_time: '1570718427',
+					noread: 3,
+					data: '你好啊~'
+				}, {
+					username: '千北',
+					avatar: '/static/default.jpg',
+					update_time: '1570718427',
+					noread: 3,
+					data: '你好啊~'
+				}]
 			};
+		},
+		// 监听下拉刷新
+		onPullDownRefresh() {
+			this.refresh()
+		},
+		// 监听原生导航栏按钮事件
+		onNavigationBarButtonTap(e) {
+			switch (e.index) {
+				case 0: // 左边
+					uni.navigateTo({
+						url: '../userList/userList',
+					});
+					// 关闭弹出层
+					this.$refs.popup.close()
+					break;
+				case 1: // 右边
+					this.$refs.popup.open()
+					break;
+			}
+		},
+		methods: {
+			// 下拉刷新
+			refresh() {
+				setTimeout(() => {
+					// this.list = demo
+					// 停止下拉刷新
+					uni.stopPullDownRefresh()
+				}, 2000)
+			},
+			// 弹出层选项点击事件
+			popupEvent(e) {
+				switch (e) {
+					case 'friend':
+						// uni.navigateTo({
+						// 	url: '../search/search?type=user',
+						// });
+						break;
+					case 'clear':
+						// this.$store.commit('clearChatList')
+						uni.showToast({
+							title: '清除成功',
+							icon: 'none'
+						});
+						console.log('清除列表');
+						break;
+				}
+				// 关闭弹出层
+				this.$refs.popup.close()
+			}
 		}
 	}
 </script>
